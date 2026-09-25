dbe9e032da0757b1e8381de21d38b3f1b31545b2
Tasawwur14 <101120848+Tasawwur14@users.noreply.github.com>
2026-09-25 22:23:08 +0530

    refactor user routes for improved readability and consistency

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,27 +5,49 @@ const app = express();
 
 app.use(express.json())
 
-app.post("/signup", async (req, res)=>{
-    const username = req.body.username 
+app.post("/signup", async (req, res) => {
+    const username = req.body.username
     const password = req.body.password
     const findUser = await User.findOne({
         username: username
     })
-    if(!findUser){
-       await User.create({
-            username : username,
-            password : password
+    if (!findUser) {
+        await User.create({
+            username: username,
+            password: password
         })
         res.json({
             message: "User Account Created Successfully"
         })
     }
-    else{
+    else {
         res.json({
             message: "Account Already Exists"
         })
     }
-
 })
+
+app.post("/login", async (req, res) => {
+        const username = req.body.username
+        const password = req.body.password
+
+        const userExisits = await User.findOne({
+            username: username,
+            password: password
+        })
+
+        if (!userExisits) {
+            res.json({
+                message: "Invalid credentials"
+            })
+        }
+        else {
+            res.json({
+                message: "Login Successfully"
+            })
+        }
+
+    })
+
 
 module.exports = app
